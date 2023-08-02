b6565a411c400c3b894131d5d616f0cf55e54223
as3545 <86479682+as3545@users.noreply.github.com>
2023-08-02 16:12:40 +0530

    snake 3.py to snake grow.py

--- snake grow.py
+++ snake grow.py
@@ -1,18 +1,19 @@
 //Code
 import pygame
 import random
+
 # initializing pygame
 pygame.init()
 
 # Colors
-white = (255, 255, 255) # rgb format
-red = (255, 0, 0)
-black = (0, 0, 0)
+background = (0, 0, 0) # rgb format
+food_color = (255, 148, 0)
+snake_color = (255,165,0)
 
 # Creating window
 screen_width = 900
 screen_height = 600
-gameWindow = pygame.display.set_mode((screen_width, screen_height))
+gameWd = pygame.display.set_mode((screen_width, screen_height))
 
 # Game Title
 pygame.display.set_caption("Coders Home")
@@ -21,13 +22,15 @@ clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 55)
 
 def text_screen(text, color, x, y):
+    #render is used to display the object
     screen_text = font.render(text, True, color)
-    gameWindow.blit(screen_text, [x,y])
+    #blit is used to copy the image and place it to another object
+    gameWd.blit(screen_text, [x,y])
 
 
-def plot_snake(gameWindow, color, snk_list, snake_size):
-    for x,y in snk_list:
-        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+def plot_snake(gameWd, color, snake_list, snake_size):
+    for x,y in snake_list:
+        pygame.draw.rect(gameWd, color, [x, y, snake_size, snake_size])
 
 # Game Loop
 def gameloop():
@@ -35,10 +38,10 @@ def gameloop():
     game_over = False
     snake_x = 45
     snake_y = 55
-    velocity_x = 0
-    velocity_y = 0
-    snk_list = []
-    snk_length = 1
+    velocity_x = 2
+    velocity_y = 2
+    snake_list = []
+    snake_length = 3
 
     food_x = random.randint(20, screen_width-20)
     food_y = random.randint(60, screen_height -20)
@@ -48,8 +51,8 @@ def gameloop():
     fps = 60   # fps = frames per second
     while not exit_game:
         if game_over:
-            gameWindow.fill(white)
-            text_screen("Game Over! Press Enter To Continue", red, 100, 250)
+            gameWd.fill(background)
+            text_screen("Game Over! Press Enter To Continue", food_color, 100, 250)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -89,27 +92,27 @@ def gameloop():
                 score +=1
                 food_x = random.randint(20, screen_width - 30)
                 food_y = random.randint(60, screen_height - 30)
-                snk_length +=5
+                snake_length +=5
 
-            gameWindow.fill(white)
-            text_screen("Score: " + str(score * 10), red, 5, 5)
-            pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
-            pygame.draw.line(gameWindow, red, (0,40), (900,40),5)
+            gameWd.fill(background)
+            text_screen("Score: " + str(score * 10),food_color, 5, 5)
+            pygame.draw.rect(gameWd, food_color, [food_x, food_y, snake_size, snake_size])
+            pygame.draw.line(gameWd, food_color, (0,40), (900,40),5)
 
             head = []
             head.append(snake_x)
             head.append(snake_y)
-            snk_list.append(head)
+            snake_list.append(head)
 
-            if len(snk_list)>snk_length:
-                del snk_list[0]
+            if len(snake_list)>snake_length:
+                del snake_list[0]
 
-            if head in snk_list[:-1]:
+            if head in snake_list[:-1]:
                 game_over = True
 
             if snake_x<0 or snake_x>screen_width-20 or snake_y<50 or snake_y>screen_height-20:
                 game_over = True
-            plot_snake(gameWindow, black, snk_list, snake_size)
+            plot_snake(gameWd, snake_color, snake_list, snake_size)
         pygame.display.update()
         clock.tick(fps)
     pygame.quit()
